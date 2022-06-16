1b042610999ecf198ebd3a578cf742b5f2b9bec7
Saif Hossain <atonlymesaif@gmail.com>
2022-06-16 13:52:33 +0600

    update endpoint created

--- src/routes/crud.router.js
+++ src/routes/crud.router.js
@@ -16,12 +16,31 @@ crudRouter.post('/post', async (req, res) => {
 
 crudRouter.delete('/delete', async (req, res) => {
     try {
-        const object = await CRUDObject.findOneAndDelete({_id: req.body.id});
+        const object = await CRUDObject.findOneAndDelete({ _id: req.body.id });
 
-      
         res.status(200).send(object);
     } catch (e) {
         res.status(404).send({ error: { status: 404, message: e.message } });
+    }
+});
+
+crudRouter.patch('/update', async (req, res) => {
+    try {
+        const filter = { _id: req.body.id };
+        const updates = { ...req.body.updates };
+        const object = await CRUDObject.findOneAndUpdate(
+            filter,
+            { data: updates },
+            {
+                new: true,
+            }
+        );
+
+        if (!object) throw new Error('Error updating document');
+
+        res.send(object);
+    } catch (e) {
+        res.status(400).send({ error: { status: 400, message: e.message } });
     }
 });
 
