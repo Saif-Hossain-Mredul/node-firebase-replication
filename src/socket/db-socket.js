67f7af2dee432c54105e440b4694d671f3a6a965
Saif Hossain <atonlymesaif@gmail.com>
2022-07-25 20:20:59 +0600

    socket connection establish error fixed

--- src/socket/db-socket.js
+++ src/socket/db-socket.js
@@ -3,19 +3,24 @@ const dbHandlers = require('./socket-handlers/db-handlers');
 
 const dbSockets = (server) => {
     // on creating new document
-    server.socket.on('post-data', dbHandlers.postDocument(server, data));
+    server.socket.on('post-data', (data) =>
+        dbHandlers.postDocument(server, data)
+    );
 
     // on updating data
-    server.socket.on(
-        'update-data',
+    server.socket.on('update-data', (updatedData) =>
         dbHandlers.updateDocument(server, updatedData)
     );
 
     // on deleting data
-    server.socket.on('delete-data', dbHandlers.deleteDocument(server, id));
+    server.socket.on('delete-data', (id) =>
+        dbHandlers.deleteDocument(server, id)
+    );
 
     // on get data
-    server.socket.on('get-data', dbHandlers.getDocuments(server, skip));
+    server.socket.on('get-data', (skip) =>
+        dbHandlers.getDocuments(server, skip)
+    );
 };
 
 module.exports = dbSockets;
