ed8fe1c19ec2531a298f829c63f368b8f7187aa8
Saif Hossain <atonlymesaif@gmail.com>
2022-08-02 10:36:21 +0600

    connection to mongoose simplified by removing other clutters

--- src/db/mongoose-connection.db.js
+++ src/db/mongoose-connection.db.js
@@ -4,31 +4,13 @@ const connectionURL = process.env.MONGODB_URL;
 
 getConnection = async () => {
     try {
-        await mongoose.connect(connectionURL, {
+        mongoose.connect(connectionURL, {
             useNewUrlParser: true,
         });
         console.log('Connection to DB Successful');
-
-        const mongooseConnection = await waitForConnection(mongoose.connection);
-
-        const collections = await mongooseConnection.db
-            .listCollections()
-            .toArray();
-        console.log(collections);
-
-        return mongooseConnection;
     } catch (err) {
         console.log(err.message);
     }
 };
 
-const waitForConnection = (connection) => {
-    return new Promise((resolve, reject) => {
-        connection.on('open', resolve(mongoose.connection));
-        connection.on('error', reject('Error connection to server'));
-    });
-};
-
-const connection = getConnection();
-
-module.exports = connection;
+getConnection();
