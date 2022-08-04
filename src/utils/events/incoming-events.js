8ca36704d4c9c74f3bbf531056edc11f8e0b511f
Saif Hossain <atonlymesaif@gmail.com>
2022-08-05 00:58:40 +0600

    incoming events are now encoded in object

---
+++ src/utils/events/incoming-events.js
@@ -0,0 +1,7 @@
+module.exports = IncomingEvents = {
+    JOIN_ROOM: 'join-room',
+    POST_DATA: 'post-data',
+    UPDATE_DATA: 'update-data',
+    DELETE_DATA: 'delete-data',
+    GET_DATA: 'get-data',
+};
