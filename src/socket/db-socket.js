bc0788d49b039daf4e127b684acd98ab1bc09ae7
Saif Hossain <atonlymesaif@gmail.com>
2022-08-05 00:59:02 +0600

    incoming events are implemented

--- src/socket/db-socket.js
+++ src/socket/db-socket.js
@@ -1,30 +1,31 @@
 const socketHandlers = require('./socket-handlers/db-handlers');
 const joinRoomHandler = require('../socket/socket-handlers/room-handlers');
+const IncomingEvents = require('../utils/events/incoming-events');
 
 const dbSockets = (server) => {
     // on joining a room and all the updated data should be send to specific
     // rooms users
-    server.socket.on('join-room', (roomInformation) =>
+    server.socket.on(IncomingEvents.JOIN_ROOM, (roomInformation) =>
         joinRoomHandler(server, roomInformation)
     );
 
     // on creating new document
-    server.socket.on('post-data', (data) =>
+    server.socket.on(IncomingEvents.POST_DATA, (data) =>
         socketHandlers.postDocument(server, data)
     );
 
     // on updating data
-    server.socket.on('update-data', (updatedData) =>
+    server.socket.on(IncomingEvents.UPDATE_DATA, (updatedData) =>
         socketHandlers.updateDocument(server, updatedData)
     );
 
     // on deleting data
-    server.socket.on('delete-data', (documentInformation) =>
+    server.socket.on(IncomingEvents.DELETE_DATA, (documentInformation) =>
         socketHandlers.deleteDocument(server, documentInformation)
     );
 
     // on get data
-    server.socket.on('get-data', (query) =>
+    server.socket.on(IncomingEvents.GET_DATA, (query) =>
         socketHandlers.getDocuments(server, query)
     );
 };
