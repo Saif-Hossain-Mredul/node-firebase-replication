675e1fabd3ef391685643f87dfb4fccdecb678c6
Saif Hossain <atonlymesaif@gmail.com>
2022-08-05 01:02:10 +0600

    outgoing events are now encoded in object

---
+++ src/utils/events/outgoing-events.js
@@ -0,0 +1,7 @@
+module.exports = OutgoingEvents = {
+    NEW_OBJECT: 'new-object',
+    UPDATED_OBJECT: 'updated-object',
+    DELETED_OBJECT: 'deleted-object',
+    RECENT_DATA: 'recent-data',
+    ERROR: 'error',
+};
